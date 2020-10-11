8c8ef1e27f9657858fc52a0d23f699e12095ce54
scorreah <68755819+scorreah@users.noreply.github.com>
2020-10-11 17:36:47 -0500

    Calculo de Gini y la mejor condición

--- proyecto/codigo/Lectura.java
+++ proyecto/codigo/Lectura.java
@@ -1,33 +1,68 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
-
+import javafx.util.Pair;
 public class Lectura { 
+    //public static ArrayList<Integer> malas = new ArrayList<Integer>(Arrays.asList(0,2,3,11,12,14,15,16,17,18,19,20,21,22,23,24,25,26,30,31,32,33,34,37,38,39,40,41,43,44,46,47,48,49,50,51,56,57,58,61,62,63,64,0));
     public static String Matriz[][]= null;
+    public static List<Integer> miLista = new ArrayList<Integer>(List.of(0, 1, 2, 3, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 30, 31, 32, 33, 34, 37, 38, 39, 40, 41, 43, 44, 46, 47, 48, 49, 50, 51, 56, 57, 58, 61, 62, 63, 64, 75, 76));
     public Lectura() throws Exception {
-        BufferedReader l1= new BufferedReader(new FileReader("0_test_balanced_5000.csv"));
-        String linea;      
+        BufferedReader l1= new BufferedReader(new FileReader("0_train_balanced_15000.csv"));
+        String linea;
         int cont = 0;
         while((linea = l1.readLine()) != null) {
             cont++; 
         }
         l1.close();
-        Lectura.Matriz=  new String[78][cont];
-        BufferedReader l2= new BufferedReader(new FileReader("0_test_balanced_5000.csv"));
+        Lectura.Matriz=  new String[cont][32];
+        BufferedReader l2= new BufferedReader(new FileReader("0_train_balanced_15000.csv"));
         int z= 0;
-        while((linea = l2.readLine())!=null){
+        int v =0;
+        while((linea=l2.readLine())!=null){
             String[] splitted = linea.split(";");
             for(int i =0; i< splitted.length; i++) {
-                Matriz[i][z]= splitted[i];
+                if(miLista.contains(i)){
+                    continue;                
+                }else{
+                    Matriz[z][v]= splitted[i];
+                    v++;
+                }
             }
+            v=0;
             z++;
         }
         l2.close();
     }
     public static void main(String[] args) throws Exception{
-        Lectura un = new Lectura();
-        for(int i = 0; i<78;i++) {
-            System.out.println(Matriz[i][10]);
-        }
+        Lectura un = new Lectura(); 
+        Node2 n = new Node2();
+        System.out.println(n.laImpurezaDeLosDatosDeUnaMatriz(Matriz));
+        Pair<Integer,String> respuesta=n.encontrarEnQuePosEstaLaMejorVariableYQueValorDeboCompararEnLaCondicion(Matriz);
+        Pair<String[][],String[][]> parejaDeMatrices = n.dividirUnaMatrizEnDosMatricesSin(Matriz, respuesta.getKey(), respuesta.getValue());
+        System.out.println(Matriz[0][respuesta.getKey()] + " " + respuesta.getValue());
+        System.out.println("IG Izq: " + n.laImpurezaDeLosDatosDeUnaMatriz(parejaDeMatrices.getKey()));
+        System.out.println("IG Der: " + n.laImpurezaDeLosDatosDeUnaMatriz(parejaDeMatrices.getValue()));
+        System.out.println("IP: " + n.calcularElGiniPonderado(parejaDeMatrices.getKey(), parejaDeMatrices.getValue()));
+        
+        System.out.println(n.laImpurezaDeLosDatosDeUnaMatriz(parejaDeMatrices.getValue()));
+        Pair<Integer,String> respuesta2=n.encontrarEnQuePosEstaLaMejorVariableYQueValorDeboCompararEnLaCondicion(parejaDeMatrices.getValue());
+        Pair<String[][],String[][]> parejaDeMatrices1 = n.dividirUnaMatrizEnDosMatricesSin(parejaDeMatrices.getValue(), respuesta2.getKey(), respuesta2.getValue());
+        System.out.println(Matriz[0][respuesta2.getKey()] + " " + respuesta2.getValue());
+        System.out.println("IG Izq: " + n.laImpurezaDeLosDatosDeUnaMatriz(parejaDeMatrices1.getKey()));
+        System.out.println("IG Der: " + n.laImpurezaDeLosDatosDeUnaMatriz(parejaDeMatrices1.getValue()));
+        System.out.println("IP: " + n.calcularElGiniPonderado(parejaDeMatrices1.getKey(), parejaDeMatrices1.getValue()));
+        /*
+        Pair<Integer,String> respuesta2=n.encontrarEnQuePosEstaLaMejorVariableYQueValorDeboCompararEnLaCondicion(parejaDeMatrices.getValue());
+        Pair<String[][],String[][]> parejaDeMatrices1 = n.dividirUnaMatrizEnDosMatricesSin(parejaDeMatrices.getValue(), respuesta2.getKey(), respuesta2.getValue());
+        Pair<Integer,String> respuesta3=n.encontrarEnQuePosEstaLaMejorVariableYQueValorDeboCompararEnLaCondicion(parejaDeMatrices1.getValue());
+        Pair<String[][],String[][]> parejaDeMatrices2 = n.dividirUnaMatrizEnDosMatricesSin(parejaDeMatrices1.getValue(), respuesta3.getKey(), respuesta3.getValue());
+        Pair<Integer,String> respuesta4=n.encontrarEnQuePosEstaLaMejorVariableYQueValorDeboCompararEnLaCondicion(parejaDeMatrices2.getValue());
+        Pair<String[][],String[][]> parejaDeMatrices3 = n.dividirUnaMatrizEnDosMatricesSin(parejaDeMatrices2.getValue(), respuesta4.getKey(), respuesta4.getValue());
+        Pair<Integer,String> respuesta5=n.encontrarEnQuePosEstaLaMejorVariableYQueValorDeboCompararEnLaCondicion(parejaDeMatrices3.getValue());
+        Pair<String[][],String[][]> parejaDeMatrices4 = n.dividirUnaMatrizEnDosMatricesSin(parejaDeMatrices3.getValue(), respuesta5.getKey(), respuesta5.getValue());
+        //System.out.println(n.calcularLaImpurezaPonderada(Matriz, 2, "B+"));
+        for(int i = 0; i<32;i++) {
+            System.out.println(Matriz[2][i]);
+        } */
     }
 }
---
+++ proyecto/codigo/Node2.java
@@ -0,0 +1,339 @@
+import java.util.TreeSet;
+import javafx.util.Pair;
+public class Node2{
+    public Node2(){
+    }
+
+    public float calcularElGiniPonderado(String[][] laMatrizDelNodoDeLaIzquierda, String[][] laMatrizDelNodoDeLaDerecha){ //Gini del parametro completo
+        float elGiniDeLaMatrizDeLaIzquierda = sacarGini(laMatrizDelNodoDeLaIzquierda);
+        float elGiniDeLaMatrizDeLaDerecha = sacarGini(laMatrizDelNodoDeLaDerecha);
+        float tamIzq = (float)laMatrizDelNodoDeLaIzquierda.length;
+        float tamDer = (float)laMatrizDelNodoDeLaDerecha.length;
+        float elGiniPonderado = ((elGiniDeLaMatrizDeLaIzquierda*tamIzq) + 
+                (elGiniDeLaMatrizDeLaDerecha*tamDer)) /  (tamIzq + tamDer);
+        return elGiniPonderado;
+    }
+
+    public float sacarGini(String[][] matrizNodo) {
+        float estudiantesExito = 0;
+        float estudiantesFracaso = 0;
+        float estudiantesTotales = 0;
+        for (int fila = 0; fila < matrizNodo.length; fila++){
+            if (matrizNodo[fila][matrizNodo[0].length-1].equals("1"))
+                estudiantesExito++; //cuantos estudiantes tienen el valor
+            estudiantesTotales++;
+        }
+        estudiantesFracaso = estudiantesTotales - estudiantesExito;
+        float gini  = 1 - (estudiantesExito/estudiantesTotales)*(estudiantesExito/estudiantesTotales) 
+            - (estudiantesFracaso/estudiantesTotales)*(estudiantesFracaso/estudiantesTotales);
+        return gini;
+    }
+
+    public TreeSet<String> sacarLosValoresDiferentesSinRepetirDeUnaVariable(String[][] m, int posVariable){  //almacenar posibles respuestas de cada columna en un arbol
+        TreeSet<String> respuesta = new TreeSet();
+        for (int fila = 1; fila < m.length; fila++){ //fila 
+            if(m[fila][posVariable]==null||m[fila][posVariable].equals("")){
+                continue;
+            }
+            respuesta.add(m[fila][posVariable]);
+        }
+        return respuesta;  
+    }
+
+    public double promedioNumerico(String[][] m, int columna) {
+        double prom = 0;
+        double suma = 0;
+        double total = 0;
+        for (int fila = 1; fila < m.length; fila++) {
+            suma += Double.parseDouble(m[fila][columna]);
+            total++;
+        }
+        prom = suma / total;
+        return prom;
+    }
+
+    public Pair<Integer,String> encontrarEnQuePosEstaLaMejorVariableYQueValorDeboCompararEnLaCondicion(String[][] m){
+        float laImpurezaMenorDentreTodoElmundo = 1;
+        String elMejorValorDentreTodoElMundo = "";
+        int laPosDeLaVariableDondeEstaElMejorValor = -1;
+        TreeSet<String> valores = new TreeSet<>();
+        float impurezaPonderadaDeEstaColumnaConEsteValor;
+        float giniValor = 1.0f;
+        float giniMenor = 1.0f;
+        for (int columna = 0; columna < m[0].length - 1; columna++) { //Recorre todas las columnas excepto la del exito
+            try {
+                Double.parseDouble(m[1][columna]);
+                double promedio = promedioNumerico(m, columna);
+                NodeNumerico nodito = new NodeNumerico();
+                impurezaPonderadaDeEstaColumnaConEsteValor = nodito.calcularLaImpurezaPonderada(m, columna, promedio); 
+                if (impurezaPonderadaDeEstaColumnaConEsteValor < laImpurezaMenorDentreTodoElmundo){
+                    laImpurezaMenorDentreTodoElmundo = impurezaPonderadaDeEstaColumnaConEsteValor;  //0
+                    laPosDeLaVariableDondeEstaElMejorValor = columna;
+                    giniValor = nodito.sacarGini(m, columna, promedio);
+                    giniMenor = 1.0f;
+                    giniMenor = giniValor; //Solo consideramos el gini por encima del promedio
+                    elMejorValorDentreTodoElMundo = String.valueOf(promedio);
+                }
+            } catch(NumberFormatException e ) {
+                valores = sacarLosValoresDiferentesSinRepetirDeUnaVariable(m, columna); //Saca las posibles soluciones de la columna
+                impurezaPonderadaDeEstaColumnaConEsteValor = calcularLaImpurezaPonderada(m, columna, valores.last());
+                if (impurezaPonderadaDeEstaColumnaConEsteValor < laImpurezaMenorDentreTodoElmundo){
+                    laImpurezaMenorDentreTodoElmundo = impurezaPonderadaDeEstaColumnaConEsteValor;  //0
+                    laPosDeLaVariableDondeEstaElMejorValor = columna;                              //sexo
+                    giniMenor = 1.0f;
+                    elMejorValorDentreTodoElMundo = "";
+                    for (String unValor : valores) {
+                        giniValor = sacarGini(m, columna, unValor);
+                        if (giniValor < giniMenor) {
+                            giniMenor = giniValor;
+                            elMejorValorDentreTodoElMundo = unValor;                                        //mujer
+                        }
+                    }
+                }  
+            }
+        }   
+        Pair<Integer,String> respuesta = new Pair(laPosDeLaVariableDondeEstaElMejorValor, elMejorValorDentreTodoElMundo);  // (posSexo, mujer)
+        return respuesta;
+    }
+    // IG = 1 - (p0)^2 - (p1)^2. P0 = exito/total P1 = NoExito/total
+    public float laImpurezaDeLosDatosDeUnaMatriz(String[][] m){
+        float losQueTienenExito = 0;
+        for (int fila = 0; fila < m.length; fila++)
+            if (m[fila][m[0].length-1].equals("1"))
+                losQueTienenExito++;
+        float tamaño = (float)m.length;
+        float losQueNoTienenExito = tamaño - losQueTienenExito;
+        float proporcionDeLosQueTienenExito = losQueTienenExito/tamaño-1.0f;
+        float proporcionDeLosQueNoTieneExito = losQueNoTienenExito/tamaño-1.0f;
+        float impureza = 1 - proporcionDeLosQueTienenExito*proporcionDeLosQueTienenExito - proporcionDeLosQueNoTieneExito*proporcionDeLosQueNoTieneExito;
+        return impureza;         
+    }
+
+    public float sacarGini(String[][] m, int columna, String valor) {
+        float estudiantesExito = 0;
+        float estudiantesFracaso = 0;
+        float estudiantesTotales = 0;
+        for (int fila = 0; fila < m.length; fila++)
+            if (m[fila][columna].equalsIgnoreCase(valor)) {// por ejemplo, valor es 3, 10 SMLVM, Masculino
+                if (m[fila][m[0].length-1].equals("1"))
+                    estudiantesExito++; //cuantos estudiantes tienen el valor
+                estudiantesTotales++;
+            }
+        estudiantesFracaso = estudiantesTotales - estudiantesExito;
+        float gini  = 1.0f - (((estudiantesExito/estudiantesTotales)*(estudiantesExito/estudiantesTotales)) 
+                + ((estudiantesFracaso/estudiantesTotales)*(estudiantesFracaso/estudiantesTotales)));
+        return gini;
+    }
+    // IP = (Iizq*cuantosHayALaIzquierda + Ider*cuantosHayALaDerecha) / El numero total de datos
+    public  float calcularLaImpurezaPonderada(String[][] m, int posVariable, String valor){ //matriz m, columna, valor 
+        int enCuantosEstudiantesLaVariableEsIgualAlValor = 0;
+        for (int fila = 0; fila < m.length; fila++)
+            if (m[fila][posVariable].equals(valor))// por ejemplo, valor es 3, 10 SMLVM, Masculino
+                enCuantosEstudiantesLaVariableEsIgualAlValor++; //cuantos estudiantes tienen el valor
+        int enCuantosEstudiantesLaVariableNOEsIgualAlValor = m.length - enCuantosEstudiantesLaVariableEsIgualAlValor;  //
+        String[][] matrizLaVariableEsIgualAlValor = new String[enCuantosEstudiantesLaVariableEsIgualAlValor+1][m[0].length];
+        String[][] matrizLaVariableNOEsIgualAlValor = new String[enCuantosEstudiantesLaVariableNOEsIgualAlValor][m[0].length];
+        // Copiar los datos a las matrices
+        int fila = 0;
+        int filaN1 = 0;
+        int filaN2=0;
+        while(fila<m.length)
+        {
+            if(m[fila][posVariable].equals(valor))
+            {
+                if(filaN1==0){
+                    for (int i = 0; i < m[0].length ; i++) {
+                        matrizLaVariableEsIgualAlValor[filaN1][i] = m[0][i];
+                    }
+                    filaN1++;
+                }
+                for (int i = 0; i < m[0].length ; i++) {
+                    matrizLaVariableEsIgualAlValor[filaN1][i] = m[fila][i];
+                }
+                fila++;filaN1++;
+            }else
+            {
+                for (int i = 0; i < m[0].length ; i++) {
+                    matrizLaVariableNOEsIgualAlValor[filaN2][i] = m[fila][i];
+                }
+                fila++;filaN2++;
+            }
+        }
+        // ...
+        float impurezaPonderada = calcularElGiniPonderado(matrizLaVariableEsIgualAlValor, matrizLaVariableNOEsIgualAlValor);
+        return impurezaPonderada;
+    }
+
+    public  Pair<String[][],String[][]> dividirUnaMatrizEnDosMatrices(String[][] m, int posVariable, String valor){
+        int enCuantosEstudiantesLaVariableEsIgualAlValor = 0;
+        for (int fila = 0; fila < m.length; fila++)
+            if (m[fila][posVariable].equals(valor)) // por ejemplo, valor es 3, 10 SMLVM, Masculino
+                enCuantosEstudiantesLaVariableEsIgualAlValor++;
+        int enCuantosEstudiantesLaVariableNOEsIgualAlValor = m.length - enCuantosEstudiantesLaVariableEsIgualAlValor;
+        String[][] matrizLaVariableEsIgualAlValor = new String[enCuantosEstudiantesLaVariableEsIgualAlValor][m[0].length];
+        String[][] matrizLaVariableNOEsIgualAlValor = new String[enCuantosEstudiantesLaVariableNOEsIgualAlValor][m[0].length];
+        // Copiar los datos a las matrices
+        int fila = 0;
+        int filaN1 = 0;
+        int filaN2=0;
+        while(fila<m.length)
+        {
+            if(m[fila][posVariable].equals(valor))
+            {
+                for (int i = 0; i < m[0].length ; i++) {
+                    matrizLaVariableEsIgualAlValor[filaN1][i] = m[fila][i];
+                }
+                fila++;filaN1++;
+            }else
+            {
+                for (int i = 0; i < m[0].length ; i++) {
+                    matrizLaVariableNOEsIgualAlValor[filaN2][i] = m[fila][i];
+                }
+                fila++;filaN2++;
+            }
+        }
+        // ...
+        Pair<String[][],String[][]> parejaDeMatrices = new Pair(matrizLaVariableEsIgualAlValor,matrizLaVariableNOEsIgualAlValor);
+        return parejaDeMatrices;
+    }
+
+    public  float calcularLaImpurezaPonderada(String[][] m, int posVariable, int prom ){ //matriz m, columna, valor 
+        int enCuantosEstudiantesLaVariableEsIgualAlValor = 0;
+        for (int fila = 1; fila < m.length; fila++)
+            if (Integer.parseInt(m[fila][posVariable])>=prom)// por ejemplo, valor es 3, 10 SMLVM, Masculino
+                enCuantosEstudiantesLaVariableEsIgualAlValor++; //cuantos estudiantes tienen el valor
+        int enCuantosEstudiantesLaVariableNOEsIgualAlValor = m.length - enCuantosEstudiantesLaVariableEsIgualAlValor;  //
+        String[][] matrizLaVariableEsIgualAlValor = new String[enCuantosEstudiantesLaVariableEsIgualAlValor+1][m[0].length];
+        String[][] matrizLaVariableNOEsIgualAlValor = new String[enCuantosEstudiantesLaVariableNOEsIgualAlValor][m[0].length];
+        // Copiar los datos a las matrices
+        int fila = 0;
+        int filaN1 = 0;
+        int filaN2=0;
+        while(fila<m.length)
+        {
+            if(Integer.parseInt(m[fila][posVariable])>=prom)
+            {
+                if(filaN1==0){
+                    for (int i = 0; i < m[0].length ; i++) {
+                        matrizLaVariableEsIgualAlValor[filaN1][i] = m[0][i];
+                    }
+                    filaN1++;
+                }
+                for (int i = 0; i < m[0].length ; i++) {
+                    matrizLaVariableEsIgualAlValor[filaN1][i] = m[fila][i];
+                }
+                fila++;filaN1++;
+            }else
+            {
+                for (int i = 0; i < m[0].length ; i++) {
+                    matrizLaVariableNOEsIgualAlValor[filaN2][i] = m[fila][i];
+                }
+                fila++;filaN2++;
+            }
+        }
+        // ...
+        float impurezaPonderada = calcularElGiniPonderado(matrizLaVariableEsIgualAlValor, matrizLaVariableNOEsIgualAlValor);
+        return impurezaPonderada;
+    }
+    //---------------------------
+    public  Pair<String[][],String[][]> dividirUnaMatrizEnDosMatricesSin(String[][] m, int posVariable, String valor){
+        try{
+            double value = Double.parseDouble(valor);
+            return dividirUnaMatrizEnDosMatricesSin(m, posVariable, value);
+        }catch(NumberFormatException e ){
+            int enCuantosEstudiantesLaVariableEsIgualAlValor = 0;
+        }
+        int enCuantosEstudiantesLaVariableEsIgualAlValor = 0;
+        for (int fila = 0; fila < m.length; fila++)
+            if (m[fila][posVariable].equals(valor)) // por ejemplo, valor es 3, 10 SMLVM, Masculino
+                enCuantosEstudiantesLaVariableEsIgualAlValor++;
+        int enCuantosEstudiantesLaVariableNOEsIgualAlValor = m.length - enCuantosEstudiantesLaVariableEsIgualAlValor;
+        String[][] matrizLaVariableEsIgualAlValor = new String[enCuantosEstudiantesLaVariableEsIgualAlValor][m[0].length-1];
+        String[][] matrizLaVariableNOEsIgualAlValor = new String[enCuantosEstudiantesLaVariableNOEsIgualAlValor][m[0].length];
+        // Copiar los datos a las matrices
+        int fila = 0;
+        int filaN1 = 0;
+        int filaN2=0;
+        int v = 0;
+        while(fila<m.length)
+        {
+            if(m[fila][posVariable].equals(valor))
+            {
+                for (int i = 0; i < m[0].length ; i++) {
+                    if(i==posVariable){
+                        continue;
+                    }
+                    matrizLaVariableEsIgualAlValor[filaN1][v] = m[fila][i];
+                    v++;
+                }
+                fila++;filaN1++;
+                v=0;
+            }else
+            {
+                for (int i = 0; i < m[0].length ; i++) {
+                    matrizLaVariableNOEsIgualAlValor[filaN2][i] = m[fila][i];
+                }
+                fila++;filaN2++;
+            }
+        }
+        // ...
+        Pair<String[][],String[][]> parejaDeMatrices = new Pair(matrizLaVariableEsIgualAlValor,matrizLaVariableNOEsIgualAlValor);
+        return parejaDeMatrices;
+    }
+
+    public  Pair<String[][],String[][]> dividirUnaMatrizEnDosMatricesSin(String[][] m, int posVariable,double valor){
+        int enCuantosEstudiantesLaVariableEsIgualAlValor = 0;
+        for (int fila = 1; fila < m.length; fila++)
+            if (Double.parseDouble(m[fila][posVariable]) >= valor) // por ejemplo, valor es 3, 10 SMLVM, Masculino
+                enCuantosEstudiantesLaVariableEsIgualAlValor++;
+        int enCuantosEstudiantesLaVariableNOEsIgualAlValor = m.length - enCuantosEstudiantesLaVariableEsIgualAlValor;
+        String[][] matrizLaVariableEsIgualAlValor = new String[enCuantosEstudiantesLaVariableEsIgualAlValor+1][m[0].length-1];
+        String[][] matrizLaVariableNOEsIgualAlValor = new String[enCuantosEstudiantesLaVariableNOEsIgualAlValor][m[0].length];
+        // Copiar los datos a las matrices
+        int fila = 1;
+        int filaN1 = 0;
+        int filaN2=0;
+        int v = 0;
+        while(fila<m.length)
+        {
+            if(Double.parseDouble(m[fila][posVariable]) >= valor)
+            {
+                if(filaN1==0){
+                    for (int i = 0; i < m[0].length ; i++) {
+                        if(i==posVariable){
+                            continue;
+                        }
+                        matrizLaVariableEsIgualAlValor[filaN1][v] = m[0][i];
+                        v++;
+                    }
+                    filaN1++;
+                    v=0;
+                }
+                for (int i = 0; i < m[0].length ; i++) {
+                    if(i==posVariable){
+                        continue;
+                    }
+                    matrizLaVariableEsIgualAlValor[filaN1][v] = m[fila][i];
+                    v++;
+                }
+                fila++;filaN1++;
+                v=0;
+            }else
+            {   
+                if(filaN2==0){
+                    for (int i = 0; i < m[0].length ; i++) {
+                        matrizLaVariableNOEsIgualAlValor[filaN1][i] = m[0][i];
+                    }
+                    filaN2++;
+                }
+                for (int i = 0; i < m[0].length ; i++) {
+                    matrizLaVariableNOEsIgualAlValor[filaN2][i] = m[fila][i];
+                }
+                fila++;filaN2++;
+            }
+        }
+        // ...
+        Pair<String[][],String[][]> parejaDeMatrices = new Pair(matrizLaVariableEsIgualAlValor,matrizLaVariableNOEsIgualAlValor);
+        return parejaDeMatrices;
+    }
+}
